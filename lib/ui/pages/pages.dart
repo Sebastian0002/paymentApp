c92cf9357120847e4d419154b6e74bd9e3ff8749
jhoann martinez <jhoann022@gmail.com>
2025-01-09 10:55:09 -0500

    MVP

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,4 +1,3 @@
 export 'package:payment_app/ui/pages/cart/cart_page.dart';
 export 'package:payment_app/ui/pages/description/description_page.dart';
-export 'package:payment_app/ui/pages/succes_screen.dart';
 export 'package:payment_app/ui/pages/home/home_screen.dart';
